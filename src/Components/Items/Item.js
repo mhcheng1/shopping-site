f8f95b0d8c422c0a2e73668972ca6a56a4b24824
Meng-Hsin Cheng <david999qq@gmail.com>
2022-01-21 23:25:11 -0500

    Add pricing

--- src/Components/Items/Item.js
+++ src/Components/Items/Item.js
@@ -8,21 +8,26 @@ const CardActions1 = styled.div`
 display: flex;
 justify-content: end; `;
 
-const Item = ({ item, addToCart }) => {
 
+const Item = ({ item, addToCart }) => {
     return (
         <Card sx={{ maxWidth: 345 }}>
-                <CardMedia
-                    component="img"
-                    image={item.image.url}
-                    height="200"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+            <CardMedia
+                component="img"
+                image={item.image.url}
+                height="200"
+            />
+            <CardContent>
+                <div>
+                    <Typography gutterBottom variant="h5">
                         {item.name}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary" dangerouslySetInnerHTML={{ __html: item.description }} />
-                </CardContent>
+                    <Typography variant="h5">
+                        {item.price.formatted_with_symbol}
+                    </Typography>
+                </div>
+                <Typography variant="body2" color="text.secondary" dangerouslySetInnerHTML={{ __html: item.description }} />
+            </CardContent>
             <CardActions1>
                 <IconButton color="primary" aria-label="add to shopping cart" onClick={() => addToCart(item.id, 1)}>
                     <AddShoppingCartIcon />
