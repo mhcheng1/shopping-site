12215102940bd0d6265b33b967558a51289c9c87
Meng-Hsin Cheng <david999qq@gmail.com>
2022-01-22 16:57:06 -0500

    Update styling for price

--- src/Components/Items/Item.js
+++ src/Components/Items/Item.js
@@ -4,10 +4,19 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import styled from 'styled-components'
 
 // notes: use dangerouslySetInnerHTML to parse out HTML tags in string
+
 const CardActions1 = styled.div`
 display: flex;
-justify-content: end; `;
-
+justify-content: end; 
+`
+const end = styled.div`
+justify-content: end; 
+`
+const Description = styled.div`
+display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
 
 const Item = ({ item, addToCart }) => {
     return (
@@ -18,14 +27,14 @@ const Item = ({ item, addToCart }) => {
                 height="200"
             />
             <CardContent>
-                <div>
+                <Description>
                     <Typography gutterBottom variant="h5">
                         {item.name}
                     </Typography>
-                    <Typography variant="h5">
+                    <Typography variant="h5" className="end">
                         {item.price.formatted_with_symbol}
                     </Typography>
-                </div>
+                </Description>
                 <Typography variant="body2" color="text.secondary" dangerouslySetInnerHTML={{ __html: item.description }} />
             </CardContent>
             <CardActions1>
