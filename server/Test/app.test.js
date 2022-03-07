7272b75a835f20fd270856514a84be58828cb15e
Meng-Hsin Cheng <david999qq@gmail.com>
2022-03-06 20:57:00 -0500

    add test

---
+++ server/Test/app.test.js
@@ -0,0 +1,42 @@
+const request = require('supertest')
+const app = require('../app.js').func()
+
+describe("POST /api/insertTest", () => {
+    const sampleRequest = {
+        email: "test@gmail.com",
+        first_name: "tester"
+    }
+
+    describe("given user information: email and first_name", () => {
+        test("should response with 200 status code", async () => {
+            const response = await request(app).post("/api/insertTest").send(sampleRequest)
+            expect(response.statusCode).toBe(200)
+        })
+
+        test("should specify json in the content type header", async() => {
+            const response = await request(app).post("/api/insertTest").send(sampleRequest)
+            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+        })
+
+        test("should have valid user email and first name", async() => {
+            const response = await request(app).post("/api/insertTest").send(sampleRequest)
+            expect(response.body.email).toBeDefined()
+            expect(response.body.first_name).toBeDefined()
+        })
+    })
+})
+
+
+describe("POST /api/insertUser", () => {
+    const sampleRequest = {
+        email: "test@gmail.com",
+        first_name: "tester"
+    }
+
+    describe("given user information: email and first_name", () => {
+        test("should response with 200 status code", async () => {
+            const response = await request(app).post("/api/insertUser").send(sampleRequest)
+            expect(response.statusCode).toBe(200)
+        })
+    })
+})
